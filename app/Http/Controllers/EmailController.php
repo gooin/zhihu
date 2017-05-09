cd8488549dedf5f8d5519db80712980bebaaeefe
gooin <gooin@outlook.com>
2017-05-09 22:50:19 +0800

    use flash message when login success

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -14,6 +14,7 @@ class EmailController extends Controller
         $user = User::where('confirmation_token', $token)->first();
 
         if (is_null($user)) {
+            flash('邮箱验证失败', 'danger');
             return redirect('/');
         }
         $user->is_active = 1;
@@ -22,6 +23,7 @@ class EmailController extends Controller
         $user->save();
 
         Auth::login($user);
+        flash('邮箱验证成功', 'success');
         return redirect('/home');
     }
 }
