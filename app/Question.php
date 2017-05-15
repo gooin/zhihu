54fb9f04f11d93a61929a2cf0767775a6e068e43
gooin <gooin@outlook.com>
2017-05-15 15:27:33 +0800

    set questions and topics' relationship (many to many)

--- app/Question.php
+++ app/Question.php
@@ -8,4 +8,10 @@ class Question extends Model
 {
     //
     protected $fillable =['title', 'body', 'user_id'];
+
+    // 指定多对多联系
+    public function topics()
+    {
+        return $this->belongsToMany(Topic::class)->withTimestamps();
+    }
 }
--- app/Topic.php
+++ app/Topic.php
@@ -8,4 +8,10 @@ class Topic extends Model
 {
     //
     protected $fillable = ['name', 'questions_count'];
+
+    // 指定多对多联系
+    public function questions()
+    {
+        return $this->belongsToMany(Questions::class)->withTimestamps();
+    }
 }
